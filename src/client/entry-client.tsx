89eb2bfffc2f62d1a6294883c5f6a1f84e7cd53a
Letícia Bernardo <leticia.bernardo@mercadolivre.com>
2022-02-16 04:22:27 -0300

    Modify the common styling file name

--- src/client/entry-client.tsx
+++ src/client/entry-client.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './App';
-import '../assets/styles/index.scss';
+import '@/assets/styles/_common.scss';
 
 ReactDOM.hydrate(
   <React.StrictMode>
