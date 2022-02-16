ee3f0c3d34ed669e3edfb3a28e1d0e79dca22668
Letícia Bernardo <leticia.bernardo@mercadolivre.com>
2022-02-16 04:44:33 -0300

    Mount components to the main page for viewing

--- src/client/pages/Main.tsx
+++ src/client/pages/Main.tsx
@@ -1,13 +1,53 @@
-import React from 'react';
-import { Box } from '@chakra-ui/react';
-import { useAppContext } from '../Context';
+import { Container, Text, Box } from '@chakra-ui/react';
+import WeatherToday from '@/client/components/WeatherForecast/weatherToday';
+import WeatherNextDays from '@/client/components/WeatherForecast/weatherNextDays';
+import Header from '@/client/components/Header';
+import { useAppContext } from '@/client/Context';
 
 const Main = () => {
-  const { name } = useAppContext();
+  const { currWeatherForecast } = useAppContext();
+
   return (
-    <Box maxW="960px" mx="auto" color="grey">
-      Hello {name}!
-    </Box>
+    <Container maxW="1100px" mx="auto" width="100%" height="100%">
+      <Header />
+      {currWeatherForecast ? (
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          flexDirection="column"
+          height="80%"
+          width="100%"
+        >
+          <WeatherToday />
+          <WeatherNextDays />
+        </Box>
+      ) : (
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          flexDirection="column"
+          height="80%"
+          width="100%"
+          margin="30px 0"
+          background="#222"
+          borderRadius="20px"
+        >
+          <Box>
+            <img src="/src/assets/images/location.gif" width="140" alt="" />
+          </Box>
+          <Text
+            color="#fff"
+            fontSize="1xl"
+            marginTop="30px"
+            fontFamily="Open Sans"
+          >
+            Faça uma busca para iniciar...
+          </Text>
+        </Box>
+      )}
+    </Container>
   );
 };
 
