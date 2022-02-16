db06c3c8dbbf1b28d2f82367731eed0a5f773867
Letícia Bernardo <leticia.bernardo@mercadolivre.com>
2022-02-16 04:14:42 -0300

    Create the properties to fetch the weathers in the context

--- src/client/Context.tsx
+++ src/client/Context.tsx
@@ -1,21 +1,50 @@
+import axios from 'axios';
 import React, { useContext, useState } from 'react';
+import { app } from 'types/types';
 
 export interface Context {
-  name: string;
-  setName: (val: string) => void;
+  search: string;
+  currLocation: app.Suggestions | undefined;
+  setSearch: (val: string) => void;
+  currWeatherForecast?: app.CurrentWeatherForecast;
+  nextWeatherForecast: app.DailyWeatherForecast[];
+  fetchWeathers: (search: string) => void;
 }
-const defaultVal = {
-  name: '',
-  setName: () => {},
-} as Context;
 
-const context = React.createContext(defaultVal);
+const context = React.createContext({} as Context);
 
 const { Provider } = context;
 
 export const ContextWrapper = ({ children }: { children: any }) => {
-  const [name, setName] = useState(defaultVal.name);
-  return <Provider value={{ name, setName }}>{children}</Provider>;
+  const [search, setSearch] = useState('');
+  const [currLocation, setCurrentLocation] = useState<
+    app.Suggestions | undefined
+  >(undefined);
+  const [currWeatherForecast, setCurrWeatherForecast] = useState<
+    app.CurrentWeatherForecast | undefined
+  >(undefined);
+  const [nextWeatherForecast, setNextWeatherForecast] = useState<
+    app.DailyWeatherForecast[]
+  >([]);
+
+  function fetchWeathers(searchValue: string) {
+    console.log(searchValue);
+  }
+
+  return (
+    <Provider
+      value={{
+        currWeatherForecast,
+        nextWeatherForecast,
+        search,
+        fetchWeathers,
+        setSearch,
+        currLocation,
+      }}
+    >
+      {children}
+    </Provider>
+  );
 };
 
 export const useAppContext = () => useContext(context);
