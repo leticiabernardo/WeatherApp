73e61c703c6644ca80b3b76aecb3bf819d5d8bdc
Letícia Bernardo <leticia.bernardo@mercadolivre.com>
2022-02-16 04:35:19 -0300

    Add typescript typing

--- types/types.d.ts
+++ types/types.d.ts
@@ -1,1 +1,76 @@
-// declare modules here
+// eslint-disable-next-line @typescript-eslint/prefer-namespace-keyword
+export declare module app {
+  export interface Weather {
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+  }
+
+  export interface CurrentWeatherForecast {
+    dt: number;
+    sunrise: number;
+    sunset: number;
+    temp: number;
+    feels_like: number;
+    pressure: number;
+    humidity: number;
+    dew_point: number;
+    uvi: number;
+    clouds: number;
+    visibility: number;
+    wind_speed: number;
+    wind_deg: number;
+    weather: Weather[];
+  }
+
+  export interface DailyWeatherForecast {
+    dt: number;
+    sunrise: number;
+    sunset: number;
+    moonrise: number;
+    moonset: number;
+    moon_phase: number;
+    temp: Temp;
+    feels_like: FeelsLike;
+    pressure: number;
+    humidity: number;
+    dew_point: number;
+    wind_speed: number;
+    wind_deg: number;
+    wind_gust: number;
+    weather: Weather[];
+    clouds: number;
+    pop: number;
+    uvi: number;
+  }
+
+  export interface WeatherOneCallResponse {
+    current: CurrentWeatherForecast;
+    daily: DailyWeatherForecast[];
+  }
+
+  export interface Temp {
+    day: number;
+    min: number;
+    max: number;
+    night: number;
+    eve: number;
+    morn: number;
+  }
+
+  export interface FeelsLike {
+    day: number;
+    night: number;
+    eve: number;
+    morn: number;
+  }
+
+  export interface Suggestions {
+    name: string;
+    lat: number;
+    lon: number;
+    country: string;
+    state: string;
+  }
+}
