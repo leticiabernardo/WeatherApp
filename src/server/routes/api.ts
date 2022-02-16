0a739024a2bd32fe7f548c2c35dca226dbf09945
Letícia Bernardo <leticia.bernardo@mercadolivre.com>
2022-02-16 04:32:19 -0300

    Add a function to fetch weather forecasts

--- src/server/routes/api.ts
+++ src/server/routes/api.ts
@@ -5,6 +5,25 @@ import { app } from 'types/types';
 // TODO: move the apikey to env file
 const APIKEY = '<APIKEY>';
 
+export const getWeatherOneCall = (req: Request, res: Response) => {
+  const { lat, lon } = req.query;
+  const params = {
+    lat,
+    lon,
+    appid: APIKEY,
+    exclude: 'minutely,hourly',
+    units: 'metric',
+    lang: 'pt_br',
+  };
+  return axios
+    .get<app.WeatherOneCallResponse>(
+      'https://api.openweathermap.org/data/2.5/onecall',
+      { params }
+    )
+    .then(response => res.json(response.data).end())
+    .catch((error: Error) => error);
+};
+
 export const getSuggestions = (req: Request, res: Response) => {
   const location = req.query.search;
   const params = {
