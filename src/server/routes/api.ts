dc6b65a64ccaef6a3e15dffc8a5da7bd6786b3a6
Letícia Bernardo <leticia.bernardo@mercadolivre.com>
2022-02-16 04:30:21 -0300

    Creates a service to search for city suggestions from a word returning lat and lon

--- src/server/routes/api.ts
+++ src/server/routes/api.ts
@@ -1,11 +1,21 @@
-'use strict';
-
+import axios from 'axios';
 import { Request, Response } from 'express';
+import { app } from 'types/types';
 
-/**
- * List of API examples.
- * @route GET /api
- */
-export const getApi = async (req: Request, res: Response) => {
-  return res.status(200).end();
+// TODO: move the apikey to env file
+const APIKEY = '<APIKEY>';
+
+export const getSuggestions = (req: Request, res: Response) => {
+  const location = req.query.search;
+  const params = {
+    q: location,
+    limit: 100,
+    appid: APIKEY,
+  };
+  return axios
+    .get<app.Suggestions[]>('http://api.openweathermap.org/geo/1.0/direct', {
+      params,
+    })
+    .then(response => res.json(response.data).end())
+    .catch((error: Error) => error);
 };
