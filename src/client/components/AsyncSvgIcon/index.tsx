d30199cce56d9433112acb666475bdf87fcba622
Letícia Bernardo <leticia.bernardo@mercadolivre.com>
2022-02-16 04:23:37 -0300

    Create a component to import the Svgs asynchronously

---
+++ src/client/components/AsyncSvgIcon/index.tsx
@@ -0,0 +1,35 @@
+import { useDynamicSVGImport } from '@/client/hooks/assyncSVG';
+
+type AsyncSvgIconProps = {
+  svg: string;
+  options?: {
+    fill: string;
+    width: string;
+    height: string;
+  };
+};
+
+const defaultProps = {
+  options: {
+    fill: 'white',
+    width: '35px',
+    height: '35px',
+  },
+};
+
+const AsyncSvgIcon = ({ svg, options }: AsyncSvgIconProps) => {
+  const { SvgIcon } = useDynamicSVGImport(svg, {});
+
+  return SvgIcon ? (
+    <SvgIcon
+      fill={options?.fill}
+      width={options?.width}
+      height={options?.height}
+    />
+  ) : (
+    <div />
+  );
+};
+AsyncSvgIcon.defaultProps = defaultProps;
+
+export default AsyncSvgIcon;
---
+++ src/client/hooks/assyncSVG.tsx
@@ -0,0 +1,40 @@
+import { useRef, useState, useEffect } from 'react';
+
+interface UseDynamicSVGImportOptions {
+  onCompleted?: (
+    name: string,
+    SvgIcon: React.FC<React.SVGProps<SVGSVGElement>> | undefined
+  ) => void;
+  onError?: (err: Error) => void;
+}
+
+export function useDynamicSVGImport(
+  name: string,
+  options: UseDynamicSVGImportOptions = {}
+) {
+  const ImportedIconRef = useRef<React.FC<React.SVGProps<SVGSVGElement>>>();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<Error>();
+
+  const { onCompleted, onError } = options;
+  useEffect(() => {
+    setLoading(true);
+    const importIcon = async (): Promise<void> => {
+      try {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-member-access
+        ImportedIconRef.current = (
+          await import(`../../assets/images/${name}.svg`)
+        ).ReactComponent;
+        onCompleted?.(name, ImportedIconRef.current);
+      } catch (err) {
+        onError?.(err);
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    importIcon();
+  }, [name, onCompleted, onError]);
+
+  return { error, loading, SvgIcon: ImportedIconRef.current };
+}
